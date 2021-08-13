482c0e5b82584bfc1a64255364bafafdc45fc166
danieloralndo97 <daniel.ortiz@estudiantes.matcom.uh.cu>
2021-08-12 22:59:40 -0700

    Add option

---
+++ index.ts
@@ -0,0 +1,11 @@
+export { Result } from './result'
+export { IResultError } from './interfaces/result.error.interface'
+
+export { left, right, Either } from './either'
+
+export { Optional } from './option'
+export { IOptional } from './interfaces/option.interface'
+
+export { Guard, GuardArgumentCollection } from './guards'
+export { IGuardArgument } from './interfaces/guard-argument.interface'
+export { IGuardResult } from './interfaces/guard-result.interface'
