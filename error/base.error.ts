4040c1f0c9c5126f1de8886c90c7d918dffef202
danieloralndo97 <daniel.ortiz@estudiantes.matcom.uh.cu>
2021-08-13 00:32:05 -0700

    Add error base

---
+++ error/base.error.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { IResultError } from '../interfaces/result.error.interface';
+
+type BaseErrorProps = {
+  name: string;
+  message: string;
+  context?: string;
+};
+
+export abstract class BaseError extends Error implements IResultError {
+  constructor({ name, message, context }: BaseErrorProps) {
+    super(message);
+    // Logger.error(`${message}`, null, context);
+    Object.defineProperty(this, 'name', { value: name });
+  }
+
+  throw(): void {
+    throw this;
+  }
+
+  pretty(): string {
+    return `[${this.name}]: ${this.message}`;
+  }
+}
